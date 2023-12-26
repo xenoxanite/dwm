872d59c25f0cce84639aff2e1bfe14e326a00275
samiulbasirfahim <xenoxanite@gmail.com>
2023-12-26 19:51:51 +0600

    updated window rules

--- dwm.c
+++ dwm.c
@@ -1661,9 +1661,9 @@ loadxrdb()
       if (xrdb != NULL) {
         XRDB_LOAD_COLOR("dwm.color0", normbordercolor);
         XRDB_LOAD_COLOR("dwm.color0", normbgcolor);
-        XRDB_LOAD_COLOR("dwm.color1", normfgcolor);
-        XRDB_LOAD_COLOR("dwm.color1", selbordercolor);
-        XRDB_LOAD_COLOR("dwm.color1", selbgcolor);
+        XRDB_LOAD_COLOR("dwm.color6", normfgcolor);
+        XRDB_LOAD_COLOR("dwm.color6", selbordercolor);
+        XRDB_LOAD_COLOR("dwm.color6", selbgcolor);
         XRDB_LOAD_COLOR("dwm.color0", selfgcolor);
         // XRDB_LOAD_COLOR("dwm.color8", seltagbgcolor);
       }
