fa65aa18099012b5a70d22343ae7c5a87f93a359
samiulbasirfahim <xenoxanite@gmail.com>
2024-01-03 14:23:33 +0600

    unpatch ulinetags

--- config.def.h
+++ config.def.h
@@ -67,10 +67,6 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const char *tagsalt[] = { "󰖟", "", "", "󰙯", "", "󰎈", "󰕝", "", "" };
 static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down*/
 
-static const unsigned int ulinepad		= 0;	/* horizontal padding between the underline and tag */
-static const unsigned int ulinestroke	= 3;	/* thickness / height of the underline */
-static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
-static const int ulineall 				= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
 
 static const Rule rules[] = {
