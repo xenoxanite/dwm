69b4ca234f48d7aa09712e606cd10565185e85de
samiulbasirfahim <xenoxanite@gmail.com>
2024-01-03 14:22:12 +0600

    removed rainbow_patched

--- config.def.h
+++ config.def.h
@@ -72,17 +72,6 @@ static const unsigned int ulinestroke	= 3;	/* thickness / height of the underlin
 static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
 static const int ulineall 				= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
-static const char *tagsel[][2] = {
-	{  normfgcolor, seltagbgcolor },
-	{  normfgcolor, seltagbgcolor },
-	{  normfgcolor, seltagbgcolor },
-	{  normfgcolor, seltagbgcolor },
-	{  normfgcolor, seltagbgcolor },
-	{  normfgcolor, seltagbgcolor },
-	{  normfgcolor, seltagbgcolor },
-	{  normfgcolor, seltagbgcolor },
-	{  normfgcolor, seltagbgcolor },
-};
 
 static const Rule rules[] = {
 	/* xprop(1):
--- dwm.c
+++ dwm.c
@@ -373,7 +373,6 @@ static int restart = 0;
 static int running = 1;
 static Cur *cursor[CurLast];
 static Clr **scheme;
-static Clr **tagscheme;
 static Display *dpy;
 static Drw *drw;
 static Monitor *mons, *selmon;
@@ -1198,7 +1197,7 @@ drawbar(Monitor *m)
     if (showtags) {
       w = TEXTW(tags[i]);
       wdelta = selmon->alttag ? abs(TEXTW(tags[i]) - TEXTW(tagsalt[i])) / 2 : 0;
-      drw_setscheme(drw, (m->tagset[m->seltags] & 1 << i ? tagscheme[i] : scheme[SchemeTagsNorm]));
+      drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
       drw_text(drw, x, 0, w, bh, wdelta + lrpad / 2, (selmon->alttag ? tagsalt[i] : tags[i]), urg & 1 << i);
 		if (ulineall || m->tagset[m->seltags] & 1 << i) /* if there are conflicts, just move these lines directly underneath both 'drw_setscheme' and 'drw_text' :) */
 			drw_rect(drw, x + ulinepad, bh - ulinestroke - ulinevoffset, w - (ulinepad * 2), ulinestroke, 1, 0);
@@ -2495,15 +2494,10 @@ setup(void)
 	cursor[CurResize] = drw_cur_create(drw, XC_sizing);
 	cursor[CurMove] = drw_cur_create(drw, XC_fleur);
 	/* init appearance */
-	if (LENGTH(tags) > LENGTH(tagsel))
-		die("too few color schemes for the tags");
 	scheme = ecalloc(LENGTH(colors) + 1, sizeof(Clr *));
 	scheme[LENGTH(colors)] = drw_scm_create(drw, colors[0], 3);
 	for (i = 0; i < LENGTH(colors); i++)
 		scheme[i] = drw_scm_create(drw, colors[i], 3);
-	tagscheme = ecalloc(LENGTH(tagsel), sizeof(Clr *));
-	for (i = 0; i < LENGTH(tagsel); i++)
-		tagscheme[i] = drw_scm_create(drw, tagsel[i], 2);
 	/* init system tray */
 	updatesystray();
 	/* init bars */
