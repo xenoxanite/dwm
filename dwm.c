8f602685faf137f38ed372db0d82b31638b1b178
xenoxanite <xenoxanite@gmail.com>
2024-01-19 18:23:17 +0600

    no-border patch added

--- dwm.c
+++ dwm.c
@@ -2082,35 +2082,26 @@ resizebarwin(Monitor *m) {
 void
 resizeclient(Client *c, int x, int y, int w, int h)
 {
-	// XWindowChanges wc;
-	// c->oldx = c->x; c->x = wc.x = x;
-	// c->oldy = c->y; c->y = wc.y = y;
-	// c->oldw = c->w; c->w = wc.width = w;
-	// c->oldh = c->h; c->h = wc.height = h;
-	// wc.border_width = c->bw;
-	// if (solitary(c)) {
-	// 	c->w = wc.width += c->bw * 2;
-	// 	c->h = wc.height += c->bw * 2;
-		
-	// 		wc.border_width = 0;
-		
-	// }
-
-	// XConfigureWindow(dpy, c->win, CWX|CWY|CWWidth|CWHeight|CWBorderWidth, &wc);
-	// configure(c);
-	// XSync(dpy, False);
-
 	XWindowChanges wc;
+	unsigned int n;
+	Client *nbc;
 	c->oldx = c->x; c->x = wc.x = x;
 	c->oldy = c->y; c->y = wc.y = y;
 	c->oldw = c->w; c->w = wc.width = w;
 	c->oldh = c->h; c->h = wc.height = h;
 	wc.border_width = c->bw;
-	if ((&monocle == c->mon->lt[c->mon->sellt]->arrange) && (!c->isfloating)) {
-		wc.border_width = 0;
-		c->w = wc.width += c->bw * 2;
-		c->h = wc.height += c->bw * 2;
+
+	for (n = 0, nbc = nexttiled(c->mon->clients); nbc; nbc = nexttiled(nbc->next), n++);
+
+	if (c->isfloating || c->mon->lt[c->mon->sellt]->arrange == NULL && (&monocle == c->mon->lt[c->mon->sellt]->arrange)) {
+	} else {
+		if (c->mon->lt[c->mon->sellt]->arrange == monocle || n == 1) {
+			wc.border_width = 0;
+			c->w = wc.width += c->bw * 2;
+			c->h = wc.height += c->bw * 2;
+		}
 	}
+
 	XConfigureWindow(dpy, c->win, CWX|CWY|CWWidth|CWHeight|CWBorderWidth, &wc);
 	configure(c);
 	XSync(dpy, False);
