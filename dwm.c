0bf8c6e0c2751c83f98cc548ad9db761b33e7ae8
xenoxanite <{user}@gmail.com>
2024-02-29 22:54:22 +0600

    updated

--- dwm.c
+++ dwm.c
@@ -2094,7 +2094,7 @@ resizeclient(Client *c, int x, int y, int w, int h)
 	if (((nexttiled(c->mon->clients) == c && !nexttiled(c->next))
 	    || &monocle == c->mon->lt[c->mon->sellt]->arrange)
 	    && !c->isfullscreen && !c->isfloating
-	    && NULL != c->mon->lt[c->mon->sellt]->arrange && no_border) {
+	    && NULL != c->mon->lt[c->mon->sellt]->arrange && no_border && !(&tile == c->mon->lt[c->mon->sellt]->arrange)) {
 		c->w = wc.width += c->bw * 2;
 		c->h = wc.height += c->bw * 2;
 		wc.border_width = 0;
