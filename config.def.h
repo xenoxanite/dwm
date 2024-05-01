4a460a3cc65d66ca74846b1dcd8db4abaa6a7ad2
Xenoxanite <xenoxanite@gmail.com>
2024-05-01 13:04:43 +0600

    volume controller fix

--- config.def.h
+++ config.def.h
@@ -133,9 +133,9 @@ static const char *sprandomcmd[] = { "s",  NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const char *volumecmd[3][5] = {
-	{ "wpctl", "set-volume", "@DEFAULT_SINK@", "0.05+", "pkill", "-RTMIN+2", "dwmblocks", NULL },
-	{ "wpctl", "set-volume", "@DEFAULT_SINK@", "0.05-", "pkill", "-RTMIN+2", "dwmblocks", NULL },
-	{ "wpctl", "set-mute", "@DEFAULT_SINK@", "toggle", "pkill", "-RTMIN+2", "dwmblocks", NULL },
+	{ "wpctl", "set-volume", "@DEFAULT_SINK@", "0.05+", "&&", "pkill", "-RTMIN+2", "dwmblocks", NULL },
+	{ "wpctl", "set-volume", "@DEFAULT_SINK@", "0.05-", "&&", "pkill", "-RTMIN+2", "dwmblocks", NULL },
+	{ "wpctl", "set-mute", "@DEFAULT_SINK@", "toggle", "&&", "pkill", "-RTMIN+2", "dwmblocks", NULL },
 };
 static const char *playerctlcmd[3][3] = {
 	{ "playerctl", "play-pause", NULL },
